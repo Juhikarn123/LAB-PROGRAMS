cd9730335affcd8ce9ddf6bc6820953bca688c0a
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-05-22 19:08:05 +0530

    coin

---
+++ coin15.cpp
@@ -0,0 +1,25 @@
+/* Given an array of coins having different denominations and integer sum representing the total money.
+you have to return the fewest coins that you will need to make up that sum if it's not possible to
+construct that sum then return -1.
+
+[as infinite amount of coins each type]
+
+Example 1:
+Input:{1,2,3,4,5}
+sum=16
+output:4
+Explanation: Four coins require to make the desired sum {5,5,5,1} or {4,4,4,4}
+
+Example 2:
+Input:{1,2,5,9,8}
+sum=17
+output:2
+Explanation: Two coins require to make the desired sum [9,8]
+
+Example 3:
+Input:{2,4,6,8}
+sum=15
+output:-1
+Explanation: There is no combination present with sum 15.
+*/
+
