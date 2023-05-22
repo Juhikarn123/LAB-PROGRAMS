1027f06b56ec09070474e1080febbfc2e5cc3aea
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-05-22 19:22:12 +0530

    Create riverbridge22.cpp

---
+++ riverbridge22.cpp
@@ -0,0 +1,137 @@
+#include<iostream>
+#include<utility>
+#include<vector>
+#include<algorithm>
+#include<map>
+ 
+using namespace std;
+ 
+ 
+int lis(int a[], int n)
+{ 
+    int i, j;
+    int b[n];
+    for(i=0;i<n;i++)
+        b[i]=1;
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+          
+            if(a[i]>a[j] && b[i]<b[j]+1)
+            {
+                b[i]=b[j]+1;
+            }
+        }
+    }
+ 
+    int max=0;
+ 
+    for(i=0;i<n;i++)
+    {
+        if(b[i]>max)
+            max=b[i];
+    }
+ 
+    return max;
+}
+ 
+ 
+int main()
+{
+    int i;
+    int n;
+    int index;
+    cin>>n;
+ 
+    int first_end[n];
+    int other_end[n];
+    map<int,int> mp;
+    int reorder_first_end[n];
+ 
+    for(i=0;i<n;i++)
+    {
+        cin>>first_end[i];
+        mp[first_end[i]]=i;
+    }
+ 
+    for(i=0;i<n;i++)
+    {
+        cin>>other_end[i];
+ 
+    
+        index=mp[other_end[i]];
+        reorder_first_end[index]=i+1;
+    }
+ 
+    cout<<lis(reorder_first_end,n);
+ 
+    cout<<endl;
+    return 0;
+}
+
+
+
+/*
+#include<stdio.h>
+int lis(int a[], int n)
+{ 
+    int i, j;
+    int b[n];
+    for(i=0;i<n;i++)
+        b[i]=1;
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<i;j++)
+        {
+          
+            if(a[i]>a[j] && b[i]<b[j]+1)
+            {
+                b[i]=b[j]+1;
+            }
+        }
+    }
+ 
+    int max=0;
+ 
+    for(i=0;i<n;i++)
+    {
+        if(b[i]>max)
+            max=b[i];
+    }
+ 
+    return max;
+}
+ 
+ 
+int main()
+{
+    int i;
+    int n;
+    int index;
+    scanf("%d",&n);
+ 
+    int first_end[n];
+    int other_end[n];
+    int reorder_first_end[n];
+ 
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&first_end[i]);
+        first_end[i]=i;
+    }
+ 
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&other_end[i]);
+ 
+    
+        index=other_end[i];
+        reorder_first_end[index]=i+1;
+    }
+ 
+    printf("%d",lis(reorder_first_end,n));
+ 
+    printf("\n");
+    return 0;
+}*/
