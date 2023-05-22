97ee8288a1e23d384a66e712820e74f21b8b70c3
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-05-22 19:08:26 +0530

    supplyofcoin

---
+++ Supplyofcoin16.c
@@ -0,0 +1,22 @@
+/* Given an unlimited supply of coins given denominations,find the total number of distinct ways to get the desired change.
+ For Example,
+ Input:
+ S={1,3,5,7},target=8
+ The total number of ways is 6
+
+{1,7}
+{3,5}
+{1,1,3,3}
+{1,1,1,5}
+{1,1,1,1,1,3}
+{1,1,1,1,1,1,1,1}
+
+Input:
+S={1,2,3},target=4
+The total number of ways is 4
+{1,3}
+{2,2}
+{1,1,2}
+{1,1,1,1}
+
+*/
