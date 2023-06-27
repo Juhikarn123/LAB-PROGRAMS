b2b2793e7662aaa9945ca49a07c002d62d7c7859
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 13:41:07 +0530

    Create basic1.c

---
+++ basic1.c
@@ -0,0 +1,30 @@
+Accept a character as input and print it
+
+Write a program to accept a character as input from the user and print it.
+
+Input:
+Character
+
+Output:
+Character
+
+Sample Input 1
+a
+
+Sample Output 1
+a
+
+Sample Input 2
+Z
+
+Sample Output 2
+Z
+---------------------------------------------------------------------
+
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    printf("%c",a);
+}
