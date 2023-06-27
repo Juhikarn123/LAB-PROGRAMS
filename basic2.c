1a7021d5e77c651217a6f05cd697cb99d7c59719
Juhi Kumari <93132612+Juhikarn123@users.noreply.github.com>
2023-06-27 13:41:55 +0530

    Create basic2.c

---
+++ basic2.c
@@ -0,0 +1,29 @@
+Accept a number as input and print it
+
+Write a program to accept a number as input from the user and print it.
+
+Input:
+Integer
+
+Output:
+Integer
+
+Sample Input 1
+11
+
+Sample Output 1
+11
+
+Sample Input 2
+999
+
+Sample Output 2
+999
+----------------------------------------------------------
+  #include<stdio.h>
+int main()
+{
+    int j;
+    scanf("%d",&j);
+    printf("%d",j);
+}
